3ffa6d2009a1ca19bf7b6a07f79e4a8b5e9d8ef5
Tomislav Kukic <tomislavk.popart@gmail.com>
2024-12-27 13:47:55 +0100

    Added basic plugn definition, initial commit

---
+++ barcode-master.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Barcode Master
+Description: Simple barcode generate and render plugin
+Version: 1.0.0
+Author: Tomislav Kukic
+*/
+
+
+if (! defined('ABSPATH')) exit; // Exit if accessed directly
+
+require_once "include/classes/barcode-master-class.php";
