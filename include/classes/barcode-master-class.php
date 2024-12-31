dbc517b01ad9c46d98f2e45be10a8e6a35837ba3
Tomislav Kukic <tomislavk.popart@gmail.com>
2024-12-31 10:46:40 +0100

    Nastavak rada na klasi

--- include/classes/barcode-master-class.php
+++ include/classes/barcode-master-class.php
@@ -1,5 +1,30 @@
 <?php
 
+/**
+ * Barcode Master plugin main class
+ */
 class BarcodeMaster {
-    
+   
+    //Singleton instance of the class
+    protected static BarcodeMaster|NULL $instance = NULL;
+
+    //Protected constructor
+    protected function __construct() {}
+
+    /**
+     * Returns instance of the class and makes sure that only one instance is created
+     * 
+     * @return BarcodeMaster Instance of the class
+     */
+    public static function instance(): BarcodeMaster {
+
+        if(self::$instance === NULL) {
+            self::$instance = new BarcodeMaster();
+        }
+
+        return self::$instance;
+    }
 }
+
+
+add_action('plugin_loaded', ['BarcodeMaster', 'instance'], 30, 1);
